b5c8222d7be0a901892999a1fc403e858ce76da4
Zetahaxx <58368370+Zetahaxx@users.noreply.github.com>
2021-12-19 18:40:51 +0545

    Add files via upload

---
+++ day2ii.py
@@ -0,0 +1,25 @@
+data='''\
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+'''
+horizontal=0
+depth=0
+aim=0
+for line in data.splitlines():
+  command,n=line.split()
+  if command=="forward":
+    horizontal+=int(n)
+    depth+=aim*int(n)
+    #print(depth)
+  elif command=="down":
+    aim+=int(n)
+  else:
+    aim-=int(n)
+  
+  #print(aim,depth)
+#print(horizontal,depth)
+print(horizontal*depth)
